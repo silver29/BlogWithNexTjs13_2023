1f74ab546f8d0a3f8ab2ec338f8133c7406f4b66
becquerk29 <becquerk29@gmail.com>
2023-11-10 22:56:50 -0500

    Image priority post-hero

--- components/post/post-hero.tsx
+++ components/post/post-hero.tsx
@@ -11,7 +11,9 @@ const PostHero = ({ post, locale }: PostHeroProps) => {
     return (
         <div>
             <PostContent locale={locale} isPostPage post={post} />
-            <Image className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6" 
+            <Image 
+            priority
+            className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6" 
             src={`${process.env.NEXT_PUBLIC_ASSETS_URL}${post.image}?key=optimised`} 
             width={1280} 
             height={500} 
